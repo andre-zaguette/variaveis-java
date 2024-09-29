88c0889a81152628a70a8f7c00639f6203e00a04
André Augusto Zaguette Fernandes <36884131-andrezaguette@users.noreply.replit.com>
2024-09-29 20:29:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+
+    int numero;
+    float saldo;
+    String valor = "Descomplica - Java";
+
+    numero = 1;
+    saldo = 1.1f;
+
+    System.out.println("Hello world!");
+    System.out.println(numero);
+    System.out.println(saldo);
+    System.out.println(valor);
+    System.out.println(valor.contains("Java"));
+    System.out.println(valor.length());
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
+}
